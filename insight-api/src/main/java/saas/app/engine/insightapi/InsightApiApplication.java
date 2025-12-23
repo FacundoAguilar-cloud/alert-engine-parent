82c966ddb4dc08c9539edd40d38959ebd91e4fd5
Facundo <facundoluznyaguilar@gmail.com>
2025-12-22 21:22:12 -0300

    initial multi-module project structure

---
+++ insight-api/src/main/java/saas/app/engine/insightapi/InsightApiApplication.java
@@ -0,0 +1,13 @@
+package saas.app.engine.insightapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightApiApplication.class, args);
+    }
+
+}
---
+++ insight-api/src/test/java/saas/app/engine/insightapi/InsightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package saas.app.engine.insightapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
